938d1673f00204df26506795faa66ca87881425a
Kira Farney <kira3579@yahoo.com>
2013-03-30 14:22:08 -0400

    added categoryServices, ICategotyRepository, and ICategoryRepository. Its not complete yet. I am still working on it but wanted to have it

---
+++ TriviaGame/Domain/Services/CategoryServices.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs;
+using Application.Domain;
+using Domain.Persistence;
+using Domain.Model;
+using AutoMapper;
+
+namespace Domain.Services
+{
+    using System.Collections.Generic;
+
+    public class CategoryServices : ICategoryServices
+    {
+        private readonly ICategoryRepository _CategoryRepository;
+
+        public CategoryServices(ICategoryRepository categoryRepository) 
+        {
+            _CategoryRepository = categoryRepository;
+        }
+
+
+        public Category getCategoryByID(Int32 CategoryID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> GetCategories() 
+        {
+            Mapper.CreateMap<Category, CategoryDto>();
+
+            Category category = _CategoryRepository.GetCategories();
+
+            var categoryDtos = Mapper.Map<IEnumerable<Question>, IEnumerable<QuestionDto>>(category);
+
+            return categoryDtos;
+            
+            throw new NotImplementedException();
+        }
+    }
+}
