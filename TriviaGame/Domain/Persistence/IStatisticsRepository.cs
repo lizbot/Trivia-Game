b71259c3f0d6ef8449961509433cf771102ef113
Kira Farney <kira3579@yahoo.com>
2013-04-07 21:49:32 -0400

    started modeling statistics

--- TriviaGame/Domain/Persistence/IStatisticsRepository.cs
+++ TriviaGame/Domain/Persistence/IStatisticsRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace Domain.Persistence
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Domain.Persistence
 {
     public interface IStatisticsRepository
     {
+        public Int32 GetOverallCorrectAnswers();
+
+        public Int32 GetOverallQuestionsAttempted();
+
+        public Int32 GetGameCorrectAnswers();
+
+        public Int32 GetGameQuestionsAttempted();
+
     }
 }
