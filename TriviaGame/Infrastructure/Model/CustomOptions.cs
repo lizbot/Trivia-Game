a83b0d467410c4da62376aea59c139c085c51acb
Laura Rubio <lalis.rubiete@knights.ucf.edu>
2013-04-09 22:01:14 -0700

    Working on General Options and Custom Options initialization script

---
+++ TriviaGame/Infrastructure/Model/CustomOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Model
+{
+    class CustomOptions
+    {
+        public Boolean IsTimerOn { set; get; }
+
+        public Int32 NumberOfAnswersDisplayed { set; get; }
+
+        public Int32 CategoryId { set; get; }
+
+        public Int32 NumberOfQuestionsDesired { set; get; }
+    }
+}
